eaf8fbd7c9c3bb94a797354b1d7680186ab06fbe
Bo <boho999@hotmail.com>
2021-07-06 17:31:39 +0200

    Added multiple teams and sorting of the result

--- dice_roller.py
+++ dice_roller.py
@@ -1,21 +1,45 @@
 import random
  
+def sort_by(a):
+  return a[1]
 
 def main():
+  team_count = int(input('How many teams?'))
   dice_rolls = int(input('How many dice would you like to roll?'))
   dice_size = int(input('How many sides are the dice?'))
-  dice_sum = 0
-  for i in range(0,dice_rolls):
-    roll = random.randint(1,dice_size)
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail!')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
-    dice_sum += roll
+  results = []
 
-  print(f'You have rolled a total of {dice_sum}')  
+  for j in range (0,team_count):
+    
+    teamname = input('Teamname?')
+    
+    
+    dice_sum = 0
+    for i in range(0,dice_rolls):
+      roll = random.randint(1,dice_size)
+      if roll == 1:
+        print(f'You rolled a {roll}! Critical Fail!')
+      elif roll == dice_size:
+        print(f'You rolled a {roll}! Critical Success!')
+      else:
+        print(f'You rolled a {roll}')
+      dice_sum += roll
+
+    print(f'{teamname} have rolled a total of {dice_sum}')
+    results.append([teamname , dice_sum])
+
+  print(results)
+
+  for i in range(0, len(results)):
+    #print(i)
+    print('Team:',results[i][0],' scored ', results[i][1] ,' points')
+
+  results.sort(key=sort_by)
+  
+  for i in range(0, len(results)):
+    #print(i)
+    print('Team:',results[i][0],' scored ', results[i][1] ,' points')
+
 
 if __name__== "__main__":
   main()
